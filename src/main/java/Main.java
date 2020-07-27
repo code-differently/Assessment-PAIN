7c23e497f03d300f4e649c09ee337f9d36bc04ae
CourtneeL <48140227+CourtneeL@users.noreply.github.com>
2020-07-27 13:26:43 -0400

    assessment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,46 @@
 import org.apache.commons.io.IOUtils;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
-public class Main {
+public class Main 
+{
 
-    public String readRawDataToString() throws Exception{
+    public static HashMap wordCount(String paragraph)
+    {
+        HashMap<String, Integer> result = new HashMap<String, Integer>();
+
+        String[] words = paragraph.split(" ");
+        Integer count = 0;
+
+        for(String word : words)
+        {
+            if(result.containsKey(word) != true)
+            {
+                result.put(word, 1);
+            }
+            else
+            {
+                count = result.get(word);
+                result.remove(word, count);
+                count++;
+                result.put(word, count);
+            }
+        }
+
+        return result;
+    }
+
+    public String readRawDataToString() throws Exception
+    {
         ClassLoader classLoader = getClass().getClassLoader();
+
         String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
         return result;
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception
+    {
         String output = (new Main()).readRawDataToString();
         System.out.println(output);
 
