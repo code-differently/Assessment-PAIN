deaed5bbeb77a56c7f360ad7547021465fae3113
Malon Holmes <itsmalonholmes@gmail.com>
2020-07-27 13:25:39 -0400

    Well played, Tariq...well played

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,26 @@
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Main {
 
+    // Idea: split up the grocery items by  "##"
+    // Idea: add only the words that aren't "name", "price", "type" or "expiration"
+    // Idea: 2 Hashmaps: one for "name" occurences, "price" - doesn't work because
+    // there's no way to keep track of the "price" that went with the "name"
+
+    // BACKGROUND: didnt get far because of spending so much time figuring
+    // out the logic for this assessment... kept running into dead ends/holes
+    // in my pseudocode
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
-        String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
+        String result = IOUtils.toString(classLoader.getResourceAsStream("testf.txt"));
+        String[] arr = result.split("##");
+        for (String item : arr){
+            for (int idx = 0; idx < item.length(); idx++){
+
+            }
+        }
         return result;
     }
 
